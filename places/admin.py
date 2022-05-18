973480c14cf5b64824a466cbc2331799e9299ceb
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-18 16:54:01 +0300

    Add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place
+
+
+@admin.register(Place)
+class PLaceAdmin(admin.ModelAdmin):
+    pass
--- places/models.py
+++ places/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
-# Create your models here.
-print('Hello!')
+class Place(models.Model):
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Короткое описание')
+    description_long = models.TextField('Полное описание')
+    latitude = models.FloatField('Широта')
+    longitude = models.FloatField('Долгота')
+
+    def __str__(self) -> str:
+        return self.title
