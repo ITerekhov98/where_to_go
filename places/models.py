e3ceb0956652f0eb211af1d4d2326e15bec255cd
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-18 17:30:47 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -7,5 +7,27 @@ class Place(models.Model):
     latitude = models.FloatField('Широта')
     longitude = models.FloatField('Долгота')
 
+    class Meta:
+        verbose_name = 'место'
+        verbose_name_plural = 'места'
+
     def __str__(self) -> str:
         return self.title
+
+class Image(models.Model):
+    place = models.ForeignKey(
+        Place,
+        verbose_name='место',
+        related_name='images',
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField('Фото')
+    order_number = models.IntegerField('Позиция')
+
+    class Meta:
+        ordering = ['-order_number']
+        verbose_name = 'фотография'
+        verbose_name_plural = 'фотографии'
+
+    def __str__(self) -> str:
+        return f'{self.order_number} {self.place}'
