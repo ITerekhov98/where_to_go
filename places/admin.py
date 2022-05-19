056be32f758c3e1cd5499b75a8a24bae77c44e3f
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-19 17:44:33 +0300

    Add picture prewiew to admin UI

--- places/admin.py
+++ places/admin.py
@@ -1,18 +1,50 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import Place, Image
+
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = ('image', 'image_prewiew', 'order_number',)
+    readonly_fields = ['image_prewiew']
+
+    def image_prewiew(self, obj):
+        width = obj.image.width
+        height=obj.image.height
+        if height > 200:
+            ratio = 200 / height
+            height *= ratio
+            width *= ratio
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.image.url,
+            width=width,
+            height=height,
+            )
+        )
 
 @admin.register(Place)
 class PLaceAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
-    pass
+
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+    fields = ('image', 'image_prewiew', 'order_number',)
+    readonly_fields = ['image_prewiew']
+
+    def image_prewiew(self, obj):
+        width = obj.image.width
+        height=obj.image.height
+        if height > 200:
+            ratio = 200 / height
+            height *= ratio
+            width *= ratio
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.image.url,
+            width=width,
+            height=height,
+            )
+        )
