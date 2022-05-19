c2b9195d71886d5f9838d30df65a9c7c4984bd6e
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-19 15:28:12 +0300

    Created API endpoint with place info

--- places/urls.py
+++ places/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index),
+    path('places/<int:place_id>/', views.fetch_place)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- places/views.py
+++ places/views.py
@@ -1,34 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .models import Place
 
 
 def index(request):
-    places_geojson = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
+    places_geojson = {"type": "FeatureCollection", "features": []}
+    places = Place.objects.all()
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
             "type": "Point",
-            "coordinates": [37.62, 55.793676]
+            "coordinates": [place.longitude, place.latitude]
           },
           "properties": {
-            "title": "«Легенды Москвы",
+            "title": place.title,
             "placeId": "moscow_legends",
-            "detailsUrl": "/static/places/moscow_legends.json"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "/static/places/roofs24.json"
+            "detailsUrl": "something"
           }
         }
-      ]
-    }
+        places_geojson["features"].append(feature)
     return render(request, 'index.html', context={'places_geojson':places_geojson})
+
+def fetch_place(request, place_id):
+    place = get_object_or_404(Place.objects.prefetch_related('images'), pk=place_id)
+    place_serialized = {
+        "title": place.title,
+        "imgs": [image.image.url for image in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude
+        }
+    }
+    return JsonResponse(place_serialized, json_dumps_params={'ensure_ascii': False, 'indent': 2})
