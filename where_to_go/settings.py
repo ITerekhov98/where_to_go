1889e89ca03f17d002954c2d56d4d92b53301641
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-20 20:09:11 +0300

    Fix problem with collect static

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -124,10 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = env.str('STATIC_URL', default='static/')
+
+STATIC_ROOT =  os.path.join(BASE_DIR, "assets")
+STATIC_URL = 'assets/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
