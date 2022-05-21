af1237f3553a3448c2c565b3e61a24c0ef4a2523
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-21 08:24:16 +0300

    Add opportunity load new places via consol command

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,37 @@
+import json
+from django.core.management.base import BaseCommand, CommandError
+from django.core.files.base import ContentFile
+
+import requests
+
+from places.models import Place, Image
+
+class Command(BaseCommand):
+    help = 'To load place specify link with json-formatted info about'
+
+    def add_arguments(self, parser):
+        parser.add_argument('place_json', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        for link in options['place_json']:
+            response = requests.get(link)
+            response.raise_for_status()
+            place_serialized = response.json()
+            place, created = Place.objects.get_or_create(
+                title = place_serialized['title'],
+                description_short = place_serialized['description_short'],
+                description_long = place_serialized['description_long'],
+                latitude = place_serialized['coordinates']['lat'],
+                longitude = place_serialized['coordinates']['lng'],
+            )
+            if created:
+                for index, image_link in enumerate(place_serialized['imgs'], start=1):
+                    response = requests.get(image_link)
+                    response.raise_for_status()
+                    image_binary = ContentFile(response.content)
+                    image = Image(place=place)
+                    image.image.save(f'{place.title}_{index}.jpg', image_binary, save=True)
+                    image.save()
+                    # place.images.add(f'{place.title}_{index}', image_binary, save=True)
+
+            self.stdout.write(self.style.SUCCESS('Successfully added new place'))
